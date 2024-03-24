54c808baaa87acc79b48ab045b21063776d41f3f
Кристина Лосева <kristina.losev@yandex.ru>
2024-03-24 16:58:39 +0300

    Добавить метод по удалению задачи из истории и его реализацию

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -17,6 +17,11 @@ public class InMemoryHistoryManager implements HistoryManager {
     }
 
     @Override
+    public void remove(int id) {
+        history.remove(id);
+    }
+
+    @Override
     public List<Task> getHistory() {
        return history;
     }
