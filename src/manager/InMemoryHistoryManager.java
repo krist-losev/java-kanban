d6dee1cff4c2b525549f4fc142b22b074dee3657
Кристина Лосева <kristina.losev@yandex.ru>
2024-03-27 14:41:17 +0300

    Добавить cобственную реализацию списка и методы по добавлению, выводу списка через односвязный список и удалению узлов

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -2,11 +2,12 @@ package manager;
 
 import tasks.Task;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager {
     private List<Task> history = new ArrayList<>();
+    //DoubleLinkedList<Task> taskHistory = new DoubleLinkedList<>();
+    //private Map<Integer, Node> connectingTasksAndNodes = new HashMap<>();
 
     @Override
     public void add(Task task) {
@@ -24,5 +25,44 @@ public class InMemoryHistoryManager implements HistoryManager {
     @Override
     public List<Task> getHistory() {
        return history;
+    }
+
+
+}
+class DoubleLinkedList <Task> {
+    public Node<Task> head;
+    public Node <Task> tail;
+    public int size = 0;
+
+    DoubleLinkedList<Task> taskDoubleLinkedList = new DoubleLinkedList<>();
+
+
+    public void linkLast (Task task) {
+        final Node<Task> oldTail = tail;
+        final Node<Task> newTail = new Node<> (oldTail, task, null);
+        tail = newTail;
+        if (oldTail == null) {
+            head = newTail;
+        } else {
+            oldTail.next = newTail;
+            size++;
+        }
+    }
+
+    public List<Task> getTasks(Task task) {
+        taskDoubleLinkedList.linkLast(task);
+        ArrayList <Task> taskHistory= new ArrayList<>((Collection) taskDoubleLinkedList);
+        return taskHistory;
+    }
+
+    public void removeNode(Node<Task> delNode) {
+        if (delNode == head) {
+            head = delNode.next;
+        } else if (delNode == tail) {
+            tail = delNode.prev;
+        } else if (delNode != head && delNode != tail){
+            delNode.next.next = delNode.next;
+            delNode.prev.prev = delNode.prev;
+        }
     }
 }
