dd5bd297e3868770fa2c80b1246ec6ce50c7e710
Кристина Лосева <kristina.losev@yandex.ru>
2024-03-28 22:49:49 +0300

    Убрать toString

--- src/manager/Node.java
+++ src/manager/Node.java
@@ -6,27 +6,28 @@ import java.util.Objects;
 
 public class Node {
 
-    public Task task;
+    public Task data;
     public Node next;
     public Node prev;
 
-    public Node(Node prev, Task task, Node next) {
+    public Node(Node prev, Task data, Node tail) {
         this.prev = prev;
-        this.task = task;
-        this.next = next;
+        this.data = data;
+        this.next = tail;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Node node)) return false;
-        return Objects.equals(task, node.task) && Objects.equals(next, node.next) && Objects.equals(prev, node.prev);
+        return Objects.equals(data, node.data) && Objects.equals(next, node.next) && Objects.equals(prev, node.prev);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(task, next, prev);
+        return Objects.hash(data, next, prev);
     }
+
 }
 
 
