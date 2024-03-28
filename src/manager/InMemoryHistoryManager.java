4c77100eb79f05ed4946f8ac0cf1ef1728cf40a8
Кристина Лосева <kristina.losev@yandex.ru>
2024-03-28 22:50:35 +0300

    Дописать методы для двусвязного списка

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -5,23 +5,26 @@ import tasks.Task;
 import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager {
+
+    private Map<Integer, Node> taskHistory = new HashMap<>();
     private Node head;
     private Node tail;
     private int size = 0;
-    private Map<Integer, Node> taskHistory = new HashMap<>();
-
 
     @Override
     public void add(Task task) {
-        if (taskHistory.containsKey(task.getIdNumber())) {
-            removeNode(taskHistory.remove(task.getIdNumber()));
+        if (task != null) {
+            if (taskHistory.containsKey(task.getIdNumber())) {
+                remove(task.getIdNumber());
+            }
+        linkLast(task);
+        taskHistory.put(task.getIdNumber(), tail);
         }
-        taskHistory.put(task.getIdNumber(), linkLast(task));
     }
 
     @Override
     public void remove(int id) {
-        removeNode(taskHistory.remove(id));
+        removeNode(taskHistory.get(id));
     }
 
     @Override
@@ -29,25 +32,23 @@ public class InMemoryHistoryManager implements HistoryManager {
         return getTasks();
     }
 
-    public Node linkLast(Task task) {
+    public void linkLast(Task task) {
         final Node oldTail = tail;
         final Node newTail = new Node(oldTail, task, null);
         tail = newTail;
-        taskHistory.put(task.getIdNumber(), newTail);
         if (oldTail == null) {
             head = newTail;
         } else {
             oldTail.next = newTail;
             size++;
         }
-        return newTail;
     }
 
     public List<Task> getTasks() {
         List<Task> history = new ArrayList<>();
         Node currentNode = head;
         while (currentNode != null) {
-            history.add(currentNode.task);
+            history.add(currentNode.data);
             currentNode = currentNode.next;
         }
         return history;
