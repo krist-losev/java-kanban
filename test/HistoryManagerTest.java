185c33d719a41c388984df313ef5992dac58b431
Кристина Лосева <kristina.losev@yandex.ru>
2024-03-30 00:43:17 +0300

    Добавить тест на то, что при удалении сабтасков эпик становится пустым

--- test/HistoryManagerTest.java
+++ test/HistoryManagerTest.java
@@ -37,8 +37,21 @@ class HistoryManagerTest {
         assertEquals(3, history.size());
     }
 
+@Test
+    void addTask_TheTaskDoesNotChangeInHistory() {
+        Task newTask = new Task("Task1", "D1", Status.NEW, 1);
+        taskManager.addTask(newTask);
+        taskManager.getTaskById((newTask.getIdNumber()));
+        List<Task> history1 = taskManager.getHistory();
+        Task updateTask = new Task("UpdateTask1", "D1", Status.IN_PROGRESS, 1);
+        taskManager.updateTask(updateTask);
+        taskManager.getTaskById((updateTask.getIdNumber()));
+        List<Task> history = taskManager.getHistory();
+        assertNotEquals(history, history1);
+    }
+
     @Test
-    void historySizeEqualsTen() {
+    void addTask_noDublicatesInHistory() {
         Task newTask = new Task("Task1", "D1", Status.NEW, 1);
         taskManager.addTask(newTask);
         Epic newEpic = new Epic("Ep2", "D1", 2);
@@ -50,43 +63,53 @@ class HistoryManagerTest {
         taskManager.getSubtaskById(newSubtask.getIdNumber());
         taskManager.getTaskById((newTask.getIdNumber()));
         taskManager.getEpicById(newEpic.getIdNumber());
-        taskManager.getSubtaskById(newSubtask.getIdNumber());
-        taskManager.getTaskById((newTask.getIdNumber()));
-        taskManager.getEpicById(newEpic.getIdNumber());
-        taskManager.getSubtaskById(newSubtask.getIdNumber());
-        taskManager.getTaskById((newTask.getIdNumber()));
-        taskManager.getEpicById(newEpic.getIdNumber());
-        taskManager.getTaskById((newTask.getIdNumber()));
         List<Task> history = taskManager.getHistory();
-        assertEquals(10, history.size());
-    }
-
-@Test
-    void addTask_TheTaskDoesNotChangeInHistory() {
-        Task newTask = new Task("Task1", "D1", Status.NEW, 1);
-        taskManager.addTask(newTask);
-        taskManager.getTaskById((newTask.getIdNumber()));
-        List<Task> history1 = taskManager.getHistory();
-        Task updateTask = new Task("UpdateTask1", "D1", Status.IN_PROGRESS, 1);
-        taskManager.updateTask(updateTask);
-        taskManager.getTaskById((updateTask.getIdNumber()));
-        List<Task> history = taskManager.getHistory();
-        assertEquals(history, history1);
+        assertEquals(3, history.size());
     }
 
     @Test
-    void addTask_noDublicatesInHistory() {
+    void addTask_theTaskIsAddedToTheEnd() {
         Task newTask = new Task("Task1", "D1", Status.NEW, 1);
         taskManager.addTask(newTask);
         Epic newEpic = new Epic("Ep2", "D1", 2);
         taskManager.addEpic(newEpic);
-    }//тетс на проверку отсутствия дубликатов
+        Subtask newSubtask = new Subtask("St1", "D1", Status.NEW, 3, 2);
+        newEpic.addSubtasksInEpic(3);
+        taskManager.addSubtask(newSubtask);
+        taskManager.getEpicById(newEpic.getIdNumber());
+        taskManager.getSubtaskById(newSubtask.getIdNumber());
+        List<Task> history = taskManager.getHistory();
+        assertEquals(newSubtask, history.get(1), "Задача добавлена в конец.");
+    }
 
-    //тест на проверку, что добавляется в конец
+    @Test
+    void removeTaskInHistory() {
+        Task newTask = new Task("Task1", "D1", Status.NEW, 1);
+        taskManager.addTask(newTask);
+        Epic newEpic = new Epic("Ep2", "D1", 2);
+        taskManager.addEpic(newEpic);
+        Subtask newSubtask = new Subtask("St1", "D1", Status.NEW, 3, 2);
+        newEpic.addSubtasksInEpic(3);
+        taskManager.addSubtask(newSubtask);
+        taskManager.getEpicById(newEpic.getIdNumber());
+        taskManager.getSubtaskById(newSubtask.getIdNumber());
+        taskManager.getTaskById((newTask.getIdNumber()));
+        taskManager.deleteEpicById(newEpic.getIdNumber());
+        List<Task> history = taskManager.getHistory();
+        assertEquals(true, !history.contains(newEpic), "Объект удалён");
+    }
 
-    //тест, что удаление и добавление работают
-
-    //првоерить что при удалении подзадачи ее id не остаётся в эпике
+    @Test
+    void removeSubtask() {
+        Epic epic1 = new Epic("Сдать тз4", "Доделать дз и сдать его", 1);
+        taskManager.addEpic(epic1);
+        Subtask subtask0 = new Subtask("Дописать программу", "писааать", Status.DONE, 2, 1);
+        Subtask subtask1 = new Subtask("Проверить код", "Пропустить через дебагер", Status.IN_PROGRESS, 3, 1);
+        epic1.addSubtasksInEpic(2);
+        epic1.addSubtasksInEpic(3);
+        taskManager.deleteSubtask();
+        assertEquals(true, epic1.getSubtasksId().isEmpty());
+    }
 
 
 
